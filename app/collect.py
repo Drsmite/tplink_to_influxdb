31993d8fb5ad766cab0066c46d602b34899d52fa
Drsmite <32942382+Drsmite@users.noreply.github.com>
2023-12-20 06:07:11 -0600

    Update collect.py

--- app/collect.py
+++ app/collect.py
@@ -71,7 +71,7 @@ def main():
         if today_usage is False:
             today_usage = 1
         
-        #print(f"Plug: {kasa['name']} using {now_usage_w}W, today: {today_usage/1000} kWh")
+        print(f"Plug: {kasa['name']} using {now_usage_w}W, today: {today_usage/1000} kWh")
         stats[kasa['name']] = {
                 "today_usage" : today_usage,
                 "now_usage_w" : now_usage_w,
